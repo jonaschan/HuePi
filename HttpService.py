f425a0ae1c9439dcf492c894f6d1fc3741643fd0
Jonas Chan <jonas.chan@holovis.com>
2020-06-08 23:25:23 +0100

    Added in hook to create account and get available lights in the house

--- HttpService.py
+++ HttpService.py
@@ -3,27 +3,47 @@ import json
 import ipaddress
 
 class HueInterfaceController:
-	"""
-	Interfaces with the Hue Bridge.
-	"""	
-	def getHueIpAddress(self):
-		response = requests.get("https://discovery.meethue.com/")
-		return response.json()[0]["internalipaddress"]
-		
-	def getUserToken(self):
-		return "token"
-		
-	def createNewAccount(self):
-		ip = self.getHueIpAddress()
-		
-		accountUrl = "https://{}/api/".format(ip)
-		print(accountUrl)
-		accountMessageJson = {
-			'devicetype': 'jonasTest'
-		}
-		
-		response = requests.post(accountUrl, json = accountMessageJson, verify=False)
-		print(response)
-	
+    """
+    Interfaces with the Hue Bridge.
+    """
+    def __init__(self):
+        self.token = 'hPKjNr2MNZHYIJVE2PF6RmiSxhXyqcy5IOjOcj8u'
+        self.accountUrl = ''
+        self.generateBridgeToken = False
+        self.availableLightsInTheHouse = 0
+        self.fullAccessUrl = self.getFullAccessUrl()
+        
+    def getHueIpAddress(self):
+        response = requests.get("https://discovery.meethue.com/")
+        return response.json()[0]["internalipaddress"]
 
-service = HueInterfaceController().createNewAccount()
+    def createNewAccount(self):
+        ip = self.getHueIpAddress()
+        
+        self.accountUrl = "http://{}/api".format(ip)
+
+        accountMessageJson = {
+            'devicetype': 'jonasTest'
+        }
+        
+        response = requests.post(self.accountUrl, json = accountMessageJson, verify=False)
+        jsonResponse = response.json()
+        
+        try:
+            return jsonResponse[0]['success']['username']
+        except:
+            raise ValueError('Failed to Create Account with Error {}: {}'.format(jsonResponse[0]['error']['type'], jsonResponse[0]['error']['description']))
+        
+    def getFullAccessUrl(self):
+        if (self.generateBridgeToken):
+            self.token = self.createNewAccount()
+            return '{}/api/{}'.format(self.accountUrl, self.token)
+        
+        return 'http://{}/api/{}'.format(self.getHueIpAddress(), self.token)
+                    
+    def getAvailableLightsConnected(self):
+        lightsUrl = '{}/lights'.format(self.fullAccessUrl)
+        response = requests.get(lightsUrl)
+        self.availableLightsInTheHouse = len(response.json())
+        
+service = HueInterfaceController().getAvailableLightsConnected()
