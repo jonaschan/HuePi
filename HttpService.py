ad5e0fdb467c48ab1223f97719bade46369c0f08
Jonas Chan <jonas.chan@holovis.com>
2020-06-14 00:21:24 +0100

    Refactored code

--- HttpService.py
+++ HttpService.py
@@ -7,23 +7,38 @@ class HueInterfaceController:
     Interfaces with the Hue Bridge.
     """
     def __init__(self):
+        if (self.isConnectedToInternet() == False):
+            return
+        
         self.token = 'hPKjNr2MNZHYIJVE2PF6RmiSxhXyqcy5IOjOcj8u'
         self.accountUrl = ''
         self.generateBridgeToken = False
-        self.fullAccessUrl = self.getFullAccessUrl()
+        self.fullAccessUrl = self.__getFullAccessUrl()
+    
+    def isConnectedToInternet(self):
+        testUrl = "http://www.google.com/"
+        timeoutSeconds = 5
         
-    def getHueIpAddress(self):
+        try:
+            _ = requests.get(testUrl, timeout=timeoutSeconds)
+            return True
+        except requests.ConnectionError:
+            print("No internet connection! Should ping the display.")
+            
+        return False
+    
+    def __getHueIpAddress(self):
         """
         Returns the IP Address of the Hue Bridge
         """
         response = requests.get("https://discovery.meethue.com/")
         return response.json()[0]["internalipaddress"]
 
-    def createNewAccount(self):
+    def __createNewAccount(self):
         """
         Returns a token to access the Bridge.
         """
-        ip = self.getHueIpAddress()
+        ip = self.__getHueIpAddress()
         
         self.accountUrl = "http://{}/api".format(ip)
 
@@ -39,20 +54,24 @@ class HueInterfaceController:
         except:
             raise ValueError('Failed to Create Account with Error {}: {}'.format(jsonResponse[0]['error']['type'], jsonResponse[0]['error']['description']))
         
-    def getFullAccessUrl(self):
+    def __getFullAccessUrl(self):
         """
         Returns the full url to access the Bridge.
         """
         if (self.generateBridgeToken):
-            self.token = self.createNewAccount()
+            self.token = self.__createNewAccount()
             return '{}/api/{}'.format(self.accountUrl, self.token)
         
-        return 'http://{}/api/{}'.format(self.getHueIpAddress(), self.token)
+        return 'http://{}/api/{}'.format(self.__getHueIpAddress(), self.token)
                     
     def getAvailableLightsConnected(self):
         """
         Returns the available lights connected to the Bridge.
+        Returns false if the device is not connected to the internet.
         """
+        if (self.isConnectedToInternet() == False):
+            return;            
+        
         url = '{}/lights'.format(self.fullAccessUrl)
         response = requests.get(url)
         
@@ -61,7 +80,11 @@ class HueInterfaceController:
     def toggleHueLights(self, lightId, state):
         """
         Toggles the lights according to the light id. Returns true if successful.
+        Returns false if the device is not connected to the internet.
         """
+        if (self.isConnectedToInternet() == False):
+            return;
+        
         url = '{}/lights/{}/state'.format(self.fullAccessUrl, lightId)
         
         # Python representation of the dictionary body
@@ -83,6 +106,9 @@ class HueInterfaceController:
         """
         Change the brightness of the light according to the light id.
         """
+        if (self.isConnectedToInternet() == False):
+            return;
+        
         url = '{}/lights/{}/state'.format(self.fullAccessUrl, lightId)
         
         # Sanity check so the values stay within range
@@ -104,15 +130,29 @@ class HueInterfaceController:
         
         # Python representation of the dictionary body
         # e.g. returns {'on': true} 
-        stateBody = json.dumps(pythonStateBody)
+        # pythonStateBody = json.dumps(pythonStateBody)
         
         # Make the Http request
-        response = requests.put(url, data = stateBody)
-        
+        response = requests.put(url, data = pythonStateBody)
+        print(response.status_code)
         # Get the server response
         responseCode = response.json()[0].keys()
         responseCode = next(iter(responseCode))
         
         return responseCode == 'success'
+    
+    def turnOffAllTheLights(self):
+        """
+        Turns off all the lights.
+        """
+        if (self.isConnectedToInternet() == False):
+            return;
+        
+        lightCount = self.getAvailableLightsConnected()
+        
+        for x in range(lightCount):
+            self.toggleHueLights(x, False)
 
-service = HueInterfaceController().changeHueLightBrightness(1, 254)
+
+service = HueInterfaceController().getAvailableLightsConnected()
+
